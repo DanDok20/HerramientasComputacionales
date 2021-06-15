dfa4e1b279eaeccca6190199373699b99a4573b4
DanDok20 <85905111+DanDok20@users.noreply.github.com>
2021-06-14 22:24:36 -0500

    Add files via upload

---
+++ Problema 3.py
@@ -0,0 +1,19 @@
+"""
+3. Elaborar un algoritmo que realice la transformaciónde grados
+Celsius a Fahrenheit
+"""
+
+#Diseño:
+
+"""
+Obtener grados celsius
+Convertir los grados celsius a fahrenheit = grados celsius * 9 / 5 + 32
+Mostrar resultado
+"""
+
+Celsius = eval(input("Introducir grados celsius "))
+
+Convertir_a_Fahrenheit = Celsius * (9 / 5) + 32
+
+print("En grados Fahrenheit son:", Convertir_a_Fahrenheit)
+
